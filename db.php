b0f7965fcca79c0b506f90997ae7cffe4ebac296
PXL-Houd-Van-Mij <261876991+PXL-Houd-Van-Mij@users.noreply.github.com>
2026-06-17 17:19:01 +0200

    Enhance db.php with error reporting and connection checks
    Added error reporting and connection checks for database.

--- db.php
+++ db.php
@@ -1,11 +1,19 @@
 <?php
+// Toon fouten tijdens debuggen
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// InfinityFree database gegevens
 $host = "sql303.infinityfree.com";
 $user = "if0_41967688";
 $pass = "8UgNR2U2xoL";
 $db   = "if0_41967688_db";
 
+// Verbinding maken
 $conn = mysqli_connect($host, $user, $pass, $db);
 
+// Check verbinding
 if (!$conn) {
     die("Database fout: " . mysqli_connect_error());
 }
