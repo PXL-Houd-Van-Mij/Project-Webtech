a59cea223e9f2b40f2cde710f3860fd784c2ffb2
PXL-Houd-Van-Mij <261876991+PXL-Houd-Van-Mij@users.noreply.github.com>
2026-06-17 17:09:24 +0200

    Add files via upload

---
+++ delete_specialiteit.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require "db.php";
+
+if (!isset($_SESSION["admin"])) die("Geen toegang.");
+
+$id = intval($_GET["id"]);
+
+$conn->query("DELETE FROM recept_specialiteiten WHERE specialiteit_id = $id");
+$conn->query("DELETE FROM specialiteiten WHERE id = $id");
+
+header("Location: admin_specialiteiten.php");
+exit;
---
+++ delete_tag.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require "db.php";
+
+if (!isset($_SESSION["admin"])) die("Geen toegang.");
+
+$id = intval($_GET["id"]);
+
+$conn->query("DELETE FROM recept_tags WHERE tag_id = $id");
+$conn->query("DELETE FROM tags WHERE id = $id");
+
+header("Location: admin_tags.php");
+exit;
