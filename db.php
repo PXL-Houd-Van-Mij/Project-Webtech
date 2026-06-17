228b32a558835fa742c7447aaee89ffbf5f93ebe
PXL-Houd-Van-Mij <261876991+PXL-Houd-Van-Mij@users.noreply.github.com>
2026-06-17 22:05:37 +0200

    Refactor database connection for local and online use

--- db.php
+++ db.php
@@ -1,21 +1,27 @@
 <?php
-// Toon fouten tijdens debuggen
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Detecteer of we lokaal testen (php -S localhost:8000) of online draaien.
+$host_header = $_SERVER['HTTP_HOST'] ?? '';
+$is_local = (strpos($host_header, 'localhost') !== false)
+         || (strpos($host_header, '127.0.0.1') !== false);
 
-// InfinityFree database gegevens
-$host = "sql303.infinityfree.com";
-$user = "if0_41967688";
-$pass = "8UgNR2U2xoL";
-$db   = "if0_41967688_db";
+if ($is_local) {
+    // Lokale XAMPP MySQL-database (voor testen op je eigen pc)
+    $host = "127.0.0.1";
+    $user = "root";
+    $pass = "";
+    $db   = "if0_41967688_db";
+} else {
+    // Online InfinityFree-database (voor de live website)
+    $host = "sql303.infinityfree.com";
+    $user = "if0_41967688";
+    $pass = "8UgNR2U2xoL";
+    $db   = "if0_41967688_db";
+}
 
-// Verbinding maken
 $conn = mysqli_connect($host, $user, $pass, $db);
 
-// Check verbinding
 if (!$conn) {
     die("Database fout: " . mysqli_connect_error());
 }
 ?>
-// einde T
+// IOT
